b6ec40f35afa793d446629c18c321ebaef857d58
HabibJR <smhabib.abir2030@gmail.com>
2022-07-05 11:43:36 +0200

    [T4] added signup and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'login', to:"login#index"
+  get 'signup', to:"signup#index"
   root 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
